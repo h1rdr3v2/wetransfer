42c0b4dc4aa239bb0e75ae38045e1c3201b3306a
Destiny Ezenwata <ezenwatairoegbu97@gmail.com>
2025-06-29 13:12:57 +0100

    bug fix

--- index.py
+++ index.py
@@ -81,3 +81,8 @@ def submit():
         return jsonify({'status': 'success'})
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)}), 500
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
